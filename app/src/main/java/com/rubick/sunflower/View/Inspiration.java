a76dcc27a01ce114b92ff07a3126e7e71537a3ec
Node-23 <matheus.dev@hotmail.com>
2022-03-12 20:11:46 -0300

    Now the app can load an image from an Url and fit this image in the screen

--- app/src/main/java/com/rubick/sunflower/View/Inspiration.java
+++ app/src/main/java/com/rubick/sunflower/View/Inspiration.java
@@ -2,17 +2,21 @@ package com.rubick.sunflower.View;
 
 import android.os.Bundle;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bumptech.glide.Glide;
 import com.rubick.sunflower.Model.Image.CatAPI;
 import com.rubick.sunflower.R;
 import com.rubick.sunflower.Service.APIParser;
+import com.rubick.sunflower.Service.APIUrls;
 import com.rubick.sunflower.Service.PreferenceData;
 
 public class Inspiration extends AppCompatActivity {
     private TextView textView;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +25,14 @@ public class Inspiration extends AppCompatActivity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_inspiration);
 
-        textView = findViewById(R.id.inspirationText);
+        setItems();
         CatAPI data = APIParser.ConvertToType(PreferenceData.getText(this), CatAPI.class);
-        textView.setText(data.url);
+        Glide.with(this).load(data.url).into(imageView);
+        //textView.setText(data.url);
+    }
+
+    private void setItems(){
+        textView = findViewById(R.id.inspirationText);
+        imageView = findViewById(R.id.inspirationBackground);
     }
 }
